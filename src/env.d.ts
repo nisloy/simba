0140dd031eafc2545b889c8647f08657561a155a
nisloy <nisloyamuza@gmail.com>
2026-04-18 13:35:47 +0200

    Fixed the build issues 3

---
+++ src/env.d.ts
@@ -0,0 +1,27 @@
+/// <reference types="vite/client" />
+
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
+
+declare module '*.jpg' {
+  const src: string
+  export default src
+}
+
+declare module '*.png' {
+  const src: string
+  export default src
+}
+
+declare module '*.webp' {
+  const src: string
+  export default src
+}
+
+declare module '*.svg' {
+  const src: string
+  export default src
+}
